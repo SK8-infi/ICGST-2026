60e1d4b4948f249e86407324a04e583e8d971880
Shivansh Katiyar <shivansh.katiyar1712@gmail.com>
2026-05-05 07:41:09 +0530

    Fix typo in latestUpdates.js

--- src/data/latestUpdates.js
+++ src/data/latestUpdates.js
@@ -1,6 +1,6 @@
 export const latestUpdates = [
     {
-        text: "Registrations have been Starrted. Kindly register.",
+        text: "Registrations have been Started. Kindly register.",
         important: true,
         link: "https://cmt3.research.microsoft.com/ICGST2026/Submission/Index"
     }
