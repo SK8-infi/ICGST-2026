5322cbc20bc7a94c1bf78953aa11da0b2f724960
SK8-infi <shivansh.katiyar1712@gmail.com>
2026-07-02 16:44:48 +0530

    Update announcement for date extension

--- src/data/latestUpdates.js
+++ src/data/latestUpdates.js
@@ -1,6 +1,6 @@
 export const latestUpdates = [
     {
-        text: "Registrations have been Started. Kindly register.",
+        text: "Paper submission deadline has been extended to July 31, 2026.",
         important: true,
         link: "https://cmt3.research.microsoft.com/ICGST2026/Submission/Index"
     }
